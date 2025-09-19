e1bd0713130e9bc4747d200255bdd3fccf485e84
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-09-18 21:24:32 -0600

    Updated all pages to use link instead of anchor tags

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <p>CPRG 306: Web Development 2 - Assignments</p>
-        <a href="./week-2" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go to Week-2</a>
+        <Link href="/week-2" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        Go to Week-2
+        </Link>
       </main>
     </div>
   );
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import StudentInfo from "./student-info.js";
 
 export default function Page() {
   return (
     <main>
+        <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="/">←</Link>
         <h1>Shopping List</h1>
         <StudentInfo></StudentInfo>
     </main>
