83d5845a6fa3ec8a05cf44845b4c21820b234c44
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-09-25 21:45:57 -0600

    Fixed styling for multiple pages and fixed component rendering issue
    Updated styling of items to tailwind instead of material ui
    
    Used use client for all pages to fix title card issue

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,29 +1,15 @@
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
 import Chip from '@mui/material/Chip'
 
 export default function Item({ name, quantity, category }) {
   return (
     <li className="m-2 w-full flex justify-center text-center">
-      <Card
-        sx={{
-          backgroundColor: "#415A77",
-          color: "#E0E1DD",
-          width: "100%",
-          maxWidth: "300px",
-          minHeight: "10vh",
-          borderRadius: 3,
-          padding: 2,
-        }}
-      >
-        <CardContent>
-            <Divider sx={{marginBottom: 1}}>
-              <Chip label={name} size="small" />
-            </Divider>
-            Quantity: {quantity} <br/> Category:{category}
-        </CardContent>
-      </Card>
+      <div className="bg-[#415A77] text-[#E0E1DD] w-full max-w-[300px] min-h-[12vh] rounded-lg p-2 pt-3 shadow-lg">
+        <Divider sx={{marginBottom: 1}}>
+          <Chip label={name} size="small" sx={{backgroundColor: '#1B263B', color:'#E0E1DD'}}/>
+        </Divider>
+        Quantity: {quantity} <br/> Category: {category}
+      </div>
     </li>
   );
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -8,10 +8,11 @@ import {items} from "../week-3/item-list";
 export default function Page(){
     return(
     <main className='flex justify-center p-6'>
-        <Card sx={{ backgroundColor: "#0D1B2A", color: "white", 
-        width: "100%", minHeight: "90vh", borderRadius: 3, padding: 1, paddingTop: 15, marginTop: 4}}>
+        <Card sx={{ backgroundColor: "#778DA9", color: "white", 
+        width: "100%", minHeight: "90vh", borderRadius: 3, padding: 1, paddingTop: 12, marginTop: 5.5}}>
+            <h1 className='underline font-bold text-center'>Shopping List</h1>
             <CardContent>
-                <ul className="flex flex-col items-center">
+                <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 items-stretch">
                     {items.map((item, index) => (
                         <Item key={index} {...item} />
                     ))
