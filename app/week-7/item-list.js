93b16e8b39b19a5f24b37deb5815eb3967db7189
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-10-19 22:08:44 -0600

    Replicated files from week 5 and 6

---
+++ app/week-7/item-list.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import Item from "./item";
+import items from "./item.json" assert { type:'json' };
+
+
+export default function ItemList(){
+
+    const [sortBy, setSortBy] = useState("name");
+
+    const sortedList = [...items].sort((a,b) => {
+        if(a[sortBy] < b[sortBy]) return -1;
+        if(a[sortBy] > b[sortBy]) return 1;
+        return 0;
+    })
+
+    function SortName(){
+        setSortBy("name");
+    }
+    function SortCategory(){
+        setSortBy("category");
+    }
+
+    return(
+        <div>
+            <div className="bg-slate-700 grid grid-cols-1mb-10 sm:grid-cols-1 md:grid-cols-2 rounded-lg mb-10">
+                <button className={`${sortBy == "name" ? "bg-emerald-600 text-slate-200" : "bg-slate-700 text-slate-200"}  rounded-lg px-5 hover:outline-2 outline-slate-500`} 
+                onClick={SortName}>
+                    Name
+                </button>
+                <button className={`${sortBy == "category" ? "bg-emerald-600" : "bg-slate-700 text-slate-200"} rounded-lg px-5 hover:outline-2 outline-slate-500`} 
+                onClick={SortCategory}>
+                    Category
+                </button>
+            </div>
+            <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {sortedList.map((item) => (
+                    <Item key={item.id} {...item}></Item>       
+                ))}
+            </div>
+        </div>
+    )
+
+}
