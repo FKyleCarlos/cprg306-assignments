0ec45051861e63bd4243da5bbddb81876e5857d5
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-10-23 22:43:38 -0600

    Adding main page for week 7

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,15 +1,23 @@
 "use client"
-import { useState } from "react"
+import { useState } from "react";
+import itemsData from './items.json' assert { type: 'json' };
+import NewItem from "./new-item";
 import ItemList from "./item-list";
 
 export default function Page(){
 
+    const [items, setItems] = useState([...itemsData]);
+
+    function handleAddItem(newItem){
+        setItems((items) => [...items, newItem]);
+    }
+
     return(
         <main className='flex items-center justify-center p-6'>
-            <div className="flex justify-center bg-slate-300 w-full h-full rounded-lg mt-12 pt-30 pb-10 md:h-full pb-10 lg:h-[80vh]">
-                <ItemList />
+            <div className="flex-grow overflow-y-auto bg-slate-300 w-full h-[90vh] rounded-lg mt-12 p-5">
+                <NewItem onAddItem={handleAddItem} />
+                <ItemList items={items}/>
             </div>
         </main>
     )
-
 }
