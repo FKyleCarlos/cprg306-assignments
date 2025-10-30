372676b07ae0b62c958e9b762296cf5bd305ba42
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-10-29 23:24:16 -0600

    Added list rendering with added on-click for items

---
+++ app/week-8/item-list.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Item from "./item";
+
+
+export default function ItemList({items, onItemSelect}){
+
+    const [sortBy, setSortBy] = useState("name");
+
+    const sortedList = [...items].sort((a,b) => {
+        if(a[sortBy] < b[sortBy]) return -1;
+        if(a[sortBy] > b[sortBy]) return 1;
+        return 0;
+    })
+
+    function SortName(){
+        setSortBy("name");
+    }
+    function SortCategory(){
+        setSortBy("category");
+    }
+
+    return(
+        <div>
+            <div className="flex justify-center items-center">
+                <div className="bg-slate-700 grid grid-cols-1 mb-10 sm:grid-cols-1 md:grid-cols-2 rounded-lg mb-10 w-lg">
+                    <button className={`${sortBy == "name" ? "bg-emerald-600 text-slate-200" : "bg-slate-700 text-slate-200"}  rounded-lg px-5 hover:outline-2 outline-slate-500`} 
+                    onClick={SortName}>
+                        Name
+                    </button>
+                    <button className={`${sortBy == "category" ? "bg-emerald-600" : "bg-slate-700 text-slate-200"} rounded-lg px-5 hover:outline-2 outline-slate-500`} 
+                    onClick={SortCategory}>
+                        Category
+                    </button>
+                </div>
+            </div>
+            <div className="grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                {sortedList.map((item, index) => (
+                    <Item key={index} {...item} onSelect={onItemSelect}></Item>       
+                ))}
+            </div>
+        </div>
+    )
+
+}
