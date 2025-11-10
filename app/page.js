0f709f64f1fec3631686a64f31b386945a8c0ca2
FKyleCarlos <francis.kylecarlos@gmail.com>
2025-11-10 01:02:17 -0700

    Adding week-9 button

--- app/page.js
+++ app/page.js
@@ -27,6 +27,9 @@ export default function Home() {
         <Link href="/week-8" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Go to Week-8
         </Link>
+        <Link href="/week-9" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          Go to Week-9
+        </Link>
       </main>
     </div>
   );
